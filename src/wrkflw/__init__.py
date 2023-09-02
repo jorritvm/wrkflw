c37667433a42f3e9de94a519427a000182e25858
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2023-09-02 20:17:49 +0200

    add RTask class and test script

--- src/wrkflw/__init__.py
+++ src/wrkflw/__init__.py
@@ -1,6 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
+
 __version__ = version("wrkflw")
 
 from .wrkflw import Workflow
-from .tasks import SleepTask, PythonTask, ShellTask
+from .tasks import SleepTask, PythonTask, ShellTask, RTask
--- src/wrkflw/tasks.py
+++ src/wrkflw/tasks.py
@@ -2,6 +2,7 @@ import os.path
 import time
 import subprocess
 from enum import Enum
+import tempfile
 
 
 class Status(Enum):
@@ -38,7 +39,7 @@ class Task:
 
 
 class SleepTask(Task):
-    def __init__(self, name, seconds = 1):
+    def __init__(self, name, seconds=1):
         super().__init__(name)
         self.set_task(seconds)
 
@@ -64,7 +65,9 @@ class ShellTask(Task):
 
             def thunk():
                 print(f"ShellTask {self.name}: starting")
-                output = subprocess.run(shell_path, shell=True, text=True, capture_output=True)
+                output = subprocess.run(
+                    shell_path, shell=True, text=True, capture_output=True
+                )
                 last_line = output.stdout.strip().splitlines()[-1]
                 has_succeeded = "success" in last_line.lower()
                 print(f"ShellTask {self.name}: finishing")
@@ -94,3 +97,87 @@ class PythonTask(Task):
             self.task = thunk
 
 
+class RTask(Task):
+    """
+    This class allows the execution of R scripts from within Python.
+
+    Any call to Rscript.exe automatically picks up the renv
+    Commandline parameters can be passed to an RScript that is using optparse using a dictionary
+    Rscript.exe execution only works if the shell working directory is set to the R project root,
+    hence this task creates a temp batch file that takes care of that.
+    This batch file is then run as a subprocess.
+    If Rscript.exe is not on the PATH you can launch it using its full path
+
+    Attributes:
+        batchfile (str): file path of temp batch file
+        stdout (str): output from stdout
+        stderr (str): output from stderr
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.stdout = ""
+        self.stderr = ""
+        self.batchfile = ""
+
+    def set_task(
+        self,
+        r_file: str,
+        parameters: str,
+        rscript_executable: str,
+        success_string: str = "success",
+        tail_size: int = 5,
+    ):
+        if os.path.splitext(r_file)[1] == ".R":
+            super().reset_status()
+
+            # prepare wrapper arguments
+            folder, filename = os.path.split(r_file)
+            formatted_params_list = [
+                f'--{key}="{value}"' for key, value in parameters.items()
+            ]
+            formatted_params = " ".join(formatted_params_list)
+
+            # write a wrapper batch file
+            bat_file_path = self.write_batch_wrapper(
+                filename, folder, formatted_params, rscript_executable
+            )
+            self.batchfile = bat_file_path
+
+            # create a python thunk
+            def thunk():
+                print(f"RTask {self.name}: starting")
+                output = subprocess.run(
+                    bat_file_path, shell=True, text=True, capture_output=True
+                )
+                self.stderr = output.stderr
+                self.stdout = output.stdout
+
+                print("-------------- caputred job output-----------")
+                print(output)
+                last_lines = output.stdout.strip().splitlines()[-tail_size:]
+                has_succeeded = any(
+                    success_string in line.lower() for line in last_lines
+                )
+                print(f"RTask {self.name}: finishing")
+                return has_succeeded
+
+            self.task = thunk
+
+    def write_batch_wrapper(
+        self, filename, folder, formatted_params, rscript_executable
+    ):
+        # write a wrapper batch file
+        temp_dir = tempfile.mkdtemp()
+        bat_file_content = f"""
+            set rscript="{rscript_executable}"
+            set "original_dir=%cd%"
+            cd /d {folder}
+            %rscript% {filename} {formatted_params}
+            cd /d "%original_dir%"
+            """
+        bat_file_path = os.path.join(temp_dir, "temp_script.bat")
+        with open(bat_file_path, "w") as bat_file:
+            bat_file.write(bat_file_content)
+
+        return bat_file_path
